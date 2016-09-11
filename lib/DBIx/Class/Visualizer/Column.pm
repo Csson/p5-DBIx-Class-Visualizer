b3ad900149ab29abad93b05fc5be117c9871d035
Erik Carlsson <info@code301.com>
2016-09-11 11:32:34 +0000

    Classes to handle things more better

---
+++ lib/DBIx/Class/Visualizer/Column.pm
@@ -0,0 +1,141 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package DBIx::Class::Visualizer::Column;
+
+use Moo;
+use DBIx::Class::Visualizer::Relation;
+use Types::Standard qw/ArrayRef InstanceOf/;
+use PerlX::Maybe qw/provided/;
+
+has name => (
+    is => 'ro',
+    required => 1,
+);
+has data_type => (
+    is => 'ro',
+    required => 1,
+);
+has relations => (
+    is => 'ro',
+    isa => ArrayRef[InstanceOf['DBIx::Class::Visualizer::Relation']],
+    lazy => 1,
+    default => sub { [] },
+);
+
+has is_primary_key => (
+    is => 'ro',
+    default => 0,
+);
+has is_foreign_key => (
+    is => 'ro',
+    default => 0,
+);
+has is_nullable => (
+    is => 'ro',
+    default => 0,
+);
+
+has accessor => (
+    is => 'ro',
+    predicate => 1,
+);
+has size => (
+    is => 'ro',
+    predicate => 1,
+);
+has is_auto_increment => (
+    is => 'ro',
+    predicate => 1,
+);
+has is_numeric => (
+    is => 'ro',
+    predicate => 1,
+);
+has default_value => (
+    is => 'ro',
+    predicate => 1,
+);
+has sequence => (
+    is => 'ro',
+    predicate => 1,
+);
+has retrieve_on_insert => (
+    is => 'ro',
+    predicate => 1,
+);
+has unsigned => (
+    is => 'ro',
+    predicate => 1,
+);
+has extra => (
+    is => 'ro',
+    predicate => 1,
+);
+
+around BUILDARGS => sub {
+    my $orig = shift;
+    my $class = shift;
+    my %args = @_;
+
+    if(exists $args{'extra'}{'unsigned'}) {
+        $args{'unsigned'} = delete $args{'extra'}{'unsigned'};
+    }
+    # Dereference default values like \'now()'
+    if(exists $args{'default_value'}) {
+        if(ref $args{'default_value'} eq 'SCALAR') {
+            $args{'default_value'} = ${ $args{'default_value'} };
+        }
+        # ..and put quotes around string default values
+        else {
+            $args{'default_value'} = qq{'$args{'default_value'}'};
+        }
+    }
+
+    # remove things like _inflate_info
+    map { delete $args{ $_ } } grep { /^_/ } keys %args;
+
+    $class->$orig(%args);
+};
+
+sub get_reverse_relation_from {
+    my $self = shift;
+    my $destination_table = shift;
+    my $destination_column = shift;
+
+    return grep { $_->destination_table eq $destination_table && $_->destination_column eq $destination_column } @{ $self->relations };
+}
+
+sub column_name_label_tag {
+    my $self = shift;
+
+    my $column_name_tag = $self->name;
+    $column_name_tag = $self->{'is_primary_key'} ? "<b>$column_name_tag</b>" : $column_name_tag;
+    $column_name_tag = $self->{'is_foreign_key'} ? "<b>$column_name_tag</b>" : $column_name_tag;
+    return $column_name_tag;
+}
+
+sub TO_JSON {
+    my $self = shift;
+
+    return +{
+        name => $self->name,
+        data_type => $self->data_type,
+        relations => [map { $_->TO_JSON } @{ $self->relations }],
+        is_primary_key => $self->is_primary_key,
+        is_foreign_key => $self->is_foreign_key,
+        is_nullable => $self->is_nullable,
+        provided $self->has_accessor,           accessor => $self->accessor,
+        provided $self->has_size,               size => $self->size,
+        provided $self->has_is_auto_increment,  is_auto_increment => $self->is_auto_increment,
+        provided $self->has_is_numeric,         is_numeric => $self->is_numeric,
+        provided $self->has_default_value,      default_value => $self->default_value,
+        provided $self->has_sequence,           sequence => $self->sequence,
+        provided $self->has_retrieve_on_insert, retrieve_on_insert => $self->retrieve_on_insert,
+        provided $self->has_unsigned,           unsigned => $self->unsigned,
+        provided $self->has_extra,              extra => $self->extra,
+    };
+}
+
+1;
---
+++ lib/DBIx/Class/Visualizer/ResultHandler.pm
@@ -0,0 +1,146 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package DBIx::Class::Visualizer::ResultHandler;
+
+use Moo;
+use Types::Standard qw/Bool ArrayRef HashRef InstanceOf Int/;
+use PerlX::Maybe;
+use List::Util qw/any/;
+use Syntax::Keyword::Gather;
+use DBIx::Class::Visualizer::Relation;
+use DBIx::Class::Visualizer::Column;
+
+has name => (
+    is => 'ro',
+    required => 1,
+);
+has rs => (
+    is => 'ro',
+    required => 1,
+);
+has show => (
+    is => 'rw',
+    isa => Bool,
+    default => 1,
+);
+has wanted => (
+    is => 'ro',
+    isa => Bool,
+    default => 0,
+);
+has skip => (
+    is => 'ro',
+    isa => Bool,
+    default => 0,
+);
+has columns => (
+    is => 'ro',
+    isa => ArrayRef[InstanceOf['DBIx::Class::Visualizer::Column']],
+    lazy => 1,
+    builder => 1,
+);
+
+has relations => (
+    is => 'ro',
+    isa => ArrayRef[InstanceOf['DBIx::Class::Visualizer::Relation']],
+    lazy => 1,
+    builder => 1,
+);
+has _degree_of_separation => (
+    is => 'rw',
+    isa => Int,
+    init_arg => 'degree_of_separation',
+    predicate => 1,
+);
+sub degree_of_separation {
+    my $self = shift;
+    my $value = shift;
+    return 0 if !defined $value && !$self->_has_degree_of_separation;
+    return $self->_degree_of_separation if $self->_has_degree_of_separation && !defined $value;
+    return $self->_degree_of_separation if $self->_has_degree_of_separation && $value > $self->_degree_of_separation;
+    $self->_degree_of_separation($value);
+}
+
+sub _build_columns {
+    my $self = shift;
+
+    my @primary_columns = $self->rs->primary_columns;
+    my @foreign_columns = map { keys %{ $_->{'attrs'}{'fk_columns'} } } map { $self->rs->relationship_info($_) } $self->rs->relationships;
+
+    return [
+        gather {
+            for my $column_name ($self->rs->columns) {
+
+                my $column_info = $self->rs->column_info($column_name);
+                my $is_primary = (any { $column_name eq $_ } @primary_columns) ? 1 : 0;
+
+                take(DBIx::Class::Visualizer::Column->new(
+                    name => $column_name,
+                    %{ $column_info },
+                    is_primary_key => $is_primary,
+                    relations => [$self->get_relations($column_name)],
+                ));
+
+
+                #my $is_foreign = (any { $column_name eq $_ } @foreign_columns) ? 1 : 0;
+                #take +{
+                #    name => $column_name,
+                #    is_primary => (any { $column_name eq $_ } @primary_columns) ? 1 : 0,
+                #    is_foreign => (any { $column_name eq $_ } @foreign_columns) ? 1 : 0,
+                #    info => $column_info,
+                #};
+             }
+        }
+    ];
+}
+
+sub _build_relations {
+    my $self = shift;
+
+    my @relationship_names = $self->rs->relationships;
+
+    return [
+        gather {
+            RELATION:
+            for my $relation_name (@relationship_names) {
+                my $relation = $self->rs->relationship_info($relation_name);
+                next RELATION if ref $relation->{'cond'} ne 'HASH' && scalar keys %{ $relation->{'cond'} } != 1;
+
+                take(DBIx::Class::Visualizer::Relation->new(origin_table => $self->name, relation => $relation));
+            }
+        }
+    ];
+}
+
+sub get_relations {
+    my $self = shift;
+    my $column_name = shift;
+
+    return grep { $_->origin_column eq $column_name } @{ $self->relations };
+}
+
+# find the relation from $origin_column in this result_source that points to $destination_table.$destination_column
+sub get_relation_between {
+    my $self = shift;
+    my $origin_column = shift;
+    my $destination_table = shift;
+    my $destination_column = shift;
+
+    return (grep { $_->destination_table eq $destination_table && $_->destination_column eq $destination_column } $self->get_relations($origin_column))[0];
+}
+sub get_column {
+    my $self = shift;
+    my $column_name = shift;
+    return (grep { $column_name eq $_->name } @{ $self->columns })[0];
+}
+
+# Node names can't have colons in them.
+sub node_name {
+    my $node_name = shift->name;
+    $node_name =~ s{::}{__}g;
+    return $node_name;
+}
+
+1;
